6cf7f824b8d708018f0fa23f07b585b56e312c45
harryxue <gdff64538645@163.com>
2018-01-23 21:45:15 +0800

    本地没修改什么

--- src/http.js
+++ src/http.js
@@ -2,45 +2,45 @@ import axios from 'axios';
 import { Message } from 'element-ui';
 
 // 全局的 axios 默认值
-axios.defaults.timeout = 5000;  //请求花费了超过 `timeout` 的时间，请求将被中断
+axios.defaults.timeout = 5000; //请求花费了超过 `timeout` 的时间，请求将被中断
 axios.defaults.baseURL = 'http://127.0.0.1:8899/';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 //http request 拦截器
 axios.interceptors.request.use(
-        config => {
-                // const token = getCookie('名称');注意使用的时候需要引入cookie方法，推荐js-cookie
-                config.data = JSON.stringify(config.data);
-                config.headers = {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                }
-                // if(token){
-                //   config.params = {'token':token}
-                // }
-                return config;
-        },
-        error => {
-                return Promise.reject(err);
+    config => {
+        // const token = getCookie('名称');注意使用的时候需要引入cookie方法，推荐js-cookie
+        config.data = JSON.stringify(config.data);
+        config.headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
         }
+        // if(token){
+        //   config.params = {'token':token}
+        // }
+        return config;
+    },
+    error => {
+        return Promise.reject(err);
+    }
 );
 
 //http response 拦截器
 axios.interceptors.response.use(
-        response => {
-                if (response.data.errCode == 2) {
-                        router.push({
-                                path: "/login",
-                                querry: {
-                                        redirect: router.currentRoute.fullPath
-                                } //从哪个页面跳转
-                        })
-                }
-                return response;
-        },
-        error => {
-                return Promise.reject(error)
+    response => {
+        if (response.data.errCode == 2) {
+            router.push({
+                path: "/login",
+                querry: {
+                    redirect: router.currentRoute.fullPath
+                } //从哪个页面跳转
+            })
         }
+        return response;
+    },
+    error => {
+        return Promise.reject(error)
+    }
 )
 
 /**
@@ -51,17 +51,17 @@ axios.interceptors.response.use(
  */
 
 export function get(url, params = {}) {
-        return new Promise((resolve, reject) => {
-                axios.get(url, {
-                                params: params
-                        })
-                        .then(response => {
-                                resolve(response.data);
-                        })
-                        .catch(err => {
-                                reject(err)
-                        })
-        })
+    return new Promise((resolve, reject) => {
+        axios.get(url, {
+                params: params
+            })
+            .then(response => {
+                resolve(response.data);
+            })
+            .catch(err => {
+                reject(err)
+            })
+    })
 }
 
 /**
@@ -72,14 +72,14 @@ export function get(url, params = {}) {
  */
 
 export function post(url, data = {}) {
-        return new Promise((resolve, reject) => {
-                axios.post(url, data)
-                        .then(response => {
-                                resolve(response.data);
-                        }, err => {
-                                reject(err)
-                        })
-        })
+    return new Promise((resolve, reject) => {
+        axios.post(url, data)
+            .then(response => {
+                resolve(response.data);
+            }, err => {
+                reject(err)
+            })
+    })
 }
 
 /**
@@ -90,14 +90,14 @@ export function post(url, data = {}) {
  */
 
 export function patch(url, data = {}) {
-        return new Promise((resolve, reject) => {
-                axios.patch(url, data)
-                        .then(response => {
-                                resolve(response.data);
-                        }, err => {
-                                reject(err)
-                        })
-        })
+    return new Promise((resolve, reject) => {
+        axios.patch(url, data)
+            .then(response => {
+                resolve(response.data);
+            }, err => {
+                reject(err)
+            })
+    })
 }
 
 /**
@@ -108,12 +108,12 @@ export function patch(url, data = {}) {
  */
 
 export function put(url, data = {}) {
-        return new Promise((resolve, reject) => {
-                axios.put(url, data)
-                        .then(response => {
-                                resolve(response.data);
-                        }, err => {
-                                reject(err)
-                        })
-        })
+    return new Promise((resolve, reject) => {
+        axios.put(url, data)
+            .then(response => {
+                resolve(response.data);
+            }, err => {
+                reject(err)
+            })
+    })
 }
--- src/routers.js
+++ src/routers.js
@@ -1,15 +1,14 @@
 import Home from './pages/home.vue';
 import Xin from './pages/xin.vue';
 
-const routers = [
-        {
-                path: '/',
-                component: Home
-        },
-        {
-                path: '/girl',
-                name: 'home',
-                component: Xin
-        }
+const routers = [{
+        path: '/',
+        component: Home
+    },
+    {
+        path: '/girl',
+        name: 'home',
+        component: Xin
+    }
 ]
 export default routers
